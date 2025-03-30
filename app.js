41e73da4c5854aff147dab5e5a4b865f2e28502a
narutodaisuki-p <shiluobenjian3@gmail.com>
2025-03-30 17:07:33 +0900

    アプリケーションのバグ修正と依存関係の更新

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require('dotenv').config(); // dotenvを使って環境変数を読み込む
 dbUrl =process.env.DB_URL|| "mongodb://localhost:27017/yelp-camp"
 mongoose.connect(dbUrl,
     {
-        
+        ssl: true,
         
     })
     .then(() => {
